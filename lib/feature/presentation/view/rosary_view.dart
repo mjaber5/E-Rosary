f0a2d37da8448bea088d300ee9de85c734d02fc8
Mohammed Jaber <108561613+mjaber5@users.noreply.github.com>
2024-11-07 17:09:28 +0300

    app view done

--- lib/feature/presentation/view/rosary_view.dart
+++ lib/feature/presentation/view/rosary_view.dart
@@ -1,24 +1,111 @@
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:e_osary/core/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:speech_to_text/speech_to_text.dart' as stt;
 
-class RosaryView extends StatelessWidget {
+class RosaryView extends StatefulWidget {
   const RosaryView({super.key});
+
+  @override
+  State<RosaryView> createState() => _RosaryViewState();
+}
+
+class _RosaryViewState extends State<RosaryView> {
+  bool isListening = false;
+
+  late stt.SpeechToText _speechToText;
+  String text = 'Press the button & start speaking ';
+  double confidence = 1.0;
+  int counter = 0;
+  @override
+  void initState() {
+    _speechToText = stt.SpeechToText();
+    super.initState();
+  }
+
+  void incrementCounter() {
+    isListening ? counter++ : counter;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.blueGrey,
-        centerTitle: true,
-        title: const Text(
-          'E-Rosary',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+      appBar: const CustomAppBar(
+        title: 'My Rosary',
+        backgroundColor: Colors.transparent,
+        fontSize: 26,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: AvatarGlow(
+        animate: isListening,
+        glowColor: Colors.blue,
+        duration: const Duration(milliseconds: 1000),
+        repeat: true,
+        child: FloatingActionButton(
+          backgroundColor: Colors.blue,
+          onPressed: _catureVoice,
+          child: Icon(
+            isListening ? Icons.mic : Icons.mic_none,
             color: Colors.white,
+            size: 30,
           ),
         ),
       ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Text(
+                text,
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.sizeOf(context).height * 0.5),
+            Container(
+              padding: const EdgeInsets.all(20),
+              child: Center(
+                child: Text(
+                  (isListening ? counter++ : counter).toString(),
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
+  }
+
+  void _catureVoice() async {
+    if (!isListening) {
+      bool listen = await _speechToText.initialize();
+      if (listen) {
+        setState(() {
+          isListening = true;
+        });
+        _speechToText.listen(onResult: (result) {
+          setState(() {
+            text = result.recognizedWords;
+            if (result.hasConfidenceRating && result.confidence > 0) {
+              confidence = result.confidence;
+            }
+          });
+        });
+      }
+    } else {
+      setState(() {
+        isListening = false;
+        _speechToText.stop();
+      });
+    }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import firebase_core
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
