7c25960c9bd485feaf71504965430ed6c3c458a5
Mohammed Jaber <108561613+mjaber5@users.noreply.github.com>
2024-11-07 18:47:00 +0300

    app logic done

--- lib/feature/presentation/view/rosary_view.dart
+++ lib/feature/presentation/view/rosary_view.dart
@@ -1,7 +1,9 @@
+import 'dart:developer';
 import 'package:avatar_glow/avatar_glow.dart';
 import 'package:e_osary/core/widgets/app_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RosaryView extends StatefulWidget {
   const RosaryView({super.key});
@@ -12,19 +14,82 @@ class RosaryView extends StatefulWidget {
 
 class _RosaryViewState extends State<RosaryView> {
   bool isListening = false;
-
   late stt.SpeechToText _speechToText;
-  String text = 'Press the button & start speaking ';
-  double confidence = 1.0;
+  String text = 'Press the button & start speaking';
   int counter = 0;
+
+  final List<String> targetPhrases = [
+    "subhanallah",
+    "Savannahlaw",
+    "allahhuakbar",
+    "Allah hu Akbar",
+    "allahuakbar",
+    "alhamdulilah",
+    "سبحانالله",
+    "اللهأكبر",
+  ];
+
   @override
   void initState() {
-    _speechToText = stt.SpeechToText();
     super.initState();
+    _speechToText = stt.SpeechToText();
+    _loadCounter();
   }
 
-  void incrementCounter() {
-    isListening ? counter++ : counter;
+  void _incrementCounter() {
+    setState(() {
+      counter++;
+    });
+    _saveCounter();
+  }
+
+  Future<void> _saveCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('counter', counter);
+    log("Counter saved: $counter");
+  }
+
+  Future<void> _loadCounter() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      counter = prefs.getInt('counter') ?? 0;
+    });
+    log("Counter loaded: $counter");
+  }
+
+  void _captureVoice() async {
+    if (!isListening) {
+      bool listen = await _speechToText.initialize();
+      if (listen) {
+        setState(() {
+          isListening = true;
+        });
+
+        _speechToText.listen(onResult: (result) {
+          setState(() {
+            text = result.recognizedWords;
+            String normalizedText = text.replaceAll(' ', '').toLowerCase();
+            log("Recognized words: $normalizedText");
+
+            if (_matchesAnyTargetPhrase(normalizedText)) {
+              log("Match found for: $normalizedText");
+              _incrementCounter();
+            } else {
+              log("No match found for: $normalizedText");
+            }
+          });
+        });
+      }
+    } else {
+      setState(() {
+        isListening = false;
+        _speechToText.stop();
+      });
+    }
+  }
+
+  bool _matchesAnyTargetPhrase(String text) {
+    return targetPhrases.any((phrase) => text == phrase);
   }
 
   @override
@@ -35,6 +100,7 @@ class _RosaryViewState extends State<RosaryView> {
         backgroundColor: Colors.transparent,
         fontSize: 26,
       ),
+      floatingActionButtonAnimator: FloatingActionButtonAnimator.scaling,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: AvatarGlow(
         animate: isListening,
@@ -43,7 +109,7 @@ class _RosaryViewState extends State<RosaryView> {
         repeat: true,
         child: FloatingActionButton(
           backgroundColor: Colors.blue,
-          onPressed: _catureVoice,
+          onPressed: _captureVoice,
           child: Icon(
             isListening ? Icons.mic : Icons.mic_none,
             color: Colors.white,
@@ -71,9 +137,9 @@ class _RosaryViewState extends State<RosaryView> {
               padding: const EdgeInsets.all(20),
               child: Center(
                 child: Text(
-                  (isListening ? counter++ : counter).toString(),
+                  counter.toString(),
                   style: const TextStyle(
-                    fontSize: 20,
+                    fontSize: 48,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
@@ -83,29 +149,5 @@ class _RosaryViewState extends State<RosaryView> {
         ),
       ),
     );
-  }
-
-  void _catureVoice() async {
-    if (!isListening) {
-      bool listen = await _speechToText.initialize();
-      if (listen) {
-        setState(() {
-          isListening = true;
-        });
-        _speechToText.listen(onResult: (result) {
-          setState(() {
-            text = result.recognizedWords;
-            if (result.hasConfidenceRating && result.confidence > 0) {
-              confidence = result.confidence;
-            }
-          });
-        });
-      }
-    } else {
-      setState(() {
-        isListening = false;
-        _speechToText.stop();
-      });
-    }
   }
 }
